84a42f71411b0c9a15877cf5a47965ffb64ecd0e
tu4mo <tu4mo@users.noreply.github.com>
2017-12-31 13:04:40 +0200

    Limit to 1 note window, reflect open/closed status on menu bar

--- main.js
+++ main.js
@@ -5,61 +5,48 @@ const url = require('url')
 const { app, Menu, Tray } = electron
 const BrowserWindow = electron.BrowserWindow
 
-const noteWindows = []
+let appIcon = null
+let noteWindow = null
+
+const toggleNoteWindow = () => {
+  if (noteWindow) {
+    if (noteWindow.isVisible()) {
+      noteWindow.hide()
+      appIcon.setHighlightMode('never')
+    } else {
+      noteWindow.show()
+      appIcon.setHighlightMode('always')
+    }
+  } else {
+    noteWindow = new BrowserWindow({
+      alwaysOnTop: true,
+      backgroundColor: '#ffffff',
+      height: 300,
+      minimizable: false,
+      minWidth: 200,
+      frame: false,
+      maximizable: false,
+      width: 300
+    })
+
+    noteWindow.loadURL(
+      url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+      })
+    )
+
+    appIcon.setHighlightMode('always')
+  }
+}
 
 app.dock.hide()
 
+app.on('ready', () => {
+  appIcon = new Tray('icon/iconTemplate.png')
+  appIcon.on('click', toggleNoteWindow)
+})
+
 // Prevent app from closing when all windows are closed
 app.on('window-all-closed', () => {})
-
-const createTray = () => {
-  const appIcon = new Tray('icon/iconTemplate.png')
-
-  const contextMenu = Menu.buildFromTemplate([
-    { label: 'Item1', type: 'radio' },
-    { label: 'Item2', type: 'radio' }
-  ])
-
-  // Make a change to the context menu
-  contextMenu.items[1].checked = false
-
-  // Disabled for now
-  // appIcon.setContextMenu(contextMenu)
-
-  appIcon.on('click', createNoteWindow)
-}
-
-const createNoteWindow = () => {
-  const noteWindow = new BrowserWindow({
-    alwaysOnTop: true,
-    backgroundColor: '#ffffff',
-    height: 300,
-    minimizable: false,
-    minWidth: 200,
-    frame: false,
-    maximizable: false,
-    width: 300
-  })
-
-  noteWindow.loadURL(
-    url.format({
-      pathname: path.join(__dirname, 'index.html'),
-      protocol: 'file:',
-      slashes: true
-    })
-  )
-
-  noteWindow.on('closed', () => {
-    destroyNoteWindow(noteWindow)
-  })
-
-  noteWindows.push(noteWindow)
-}
-
-const destroyNoteWindow = noteWindow => {
-  const i = noteWindows.indexOf(noteWindow)
-  if (i > -1) noteWindows.splice(i, 1)
-  noteWindow = null
-}
-
-app.on('ready', createTray)
--- renderer.js
+++ renderer.js
@@ -1,11 +1,3 @@
-const remote = require('electron').remote
-
 const editor = document.getElementById('editor')
-const closeButton = document.getElementById('closeButton')
-
-closeButton.addEventListener('click', () => {
-  const window = remote.getCurrentWindow()
-  window.close()
-})
 
 editor.focus()
