685888b4d7fa0ac0fcc0f3a9827230002fe69f87
tu4mo <tu4mo@users.noreply.github.com>
2017-12-31 14:19:32 +0200

    Add context menu with Cut, Copy, Paste and Quit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require('electron')
 const path = require('path')
 const url = require('url')
 
-const { app, Menu, Tray } = electron
+const { app, Menu, MenuItem, Tray } = electron
 const BrowserWindow = electron.BrowserWindow
 
 let appIcon = null
@@ -18,27 +18,41 @@ const toggleNoteWindow = () => {
       appIcon.setHighlightMode('always')
     }
   } else {
-    noteWindow = new BrowserWindow({
-      alwaysOnTop: true,
-      backgroundColor: '#ffffff',
-      height: 300,
-      minimizable: false,
-      minWidth: 200,
-      frame: false,
-      maximizable: false,
-      width: 300
-    })
-
-    noteWindow.loadURL(
-      url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file:',
-        slashes: true
-      })
-    )
-
+    createNoteWindow()
     appIcon.setHighlightMode('always')
   }
+}
+
+const createNoteWindow = () => {
+  noteWindow = new BrowserWindow({
+    alwaysOnTop: true,
+    backgroundColor: '#ffffff',
+    height: 300,
+    minimizable: false,
+    minWidth: 200,
+    frame: false,
+    maximizable: false,
+    width: 300
+  })
+
+  noteWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    })
+  )
+
+  const contextMenu = new Menu()
+  contextMenu.append(new MenuItem({ role: 'cut' }))
+  contextMenu.append(new MenuItem({ role: 'copy' }))
+  contextMenu.append(new MenuItem({ role: 'paste' }))
+  contextMenu.append(new MenuItem({ type: 'separator' }))
+  contextMenu.append(new MenuItem({ label: 'Quit', role: 'quit' }))
+
+  noteWindow.webContents.on('context-menu', (e, params) => {
+    contextMenu.popup(noteWindow, params.x, params.y)
+  })
 }
 
 app.dock.hide()
