a3131708ab7a59acc36cfbeae148cf5701840df0
tu4mo <tu4mo@users.noreply.github.com>
2018-01-03 18:44:45 +0200

    Save and load note

--- app/renderer.js
+++ app/renderer.js
@@ -1,3 +1,13 @@
+const Store = require('electron-store')
+const store = new Store()
+
 const editor = document.getElementById('editor')
 
 editor.focus()
+
+const note = store.get('note') || ''
+editor.innerText = note
+
+editor.addEventListener('input', () => {
+  store.set('note', editor.innerText)
+})
