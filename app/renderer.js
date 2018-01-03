e0e3d6b71789640b44e93255dbbb13951a0323fb
tu4mo <tu4mo@users.noreply.github.com>
2018-01-03 20:00:14 +0200

    Debounce saving

--- app/renderer.js
+++ app/renderer.js
@@ -1,13 +1,17 @@
 const Store = require('electron-store')
+const debounce = require('lodash.debounce')
+
 const store = new Store()
 
 const editor = document.getElementById('editor')
 
 editor.focus()
 
-const note = store.get('note') || ''
+const note = store.get('note', '')
 editor.innerText = note
 
-editor.addEventListener('input', () => {
+const saveNote = () => {
   store.set('note', editor.innerText)
-})
+}
+
+editor.addEventListener('input', debounce(saveNote, 1000))
