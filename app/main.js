9953c671870979aaf8f9a3aa1680e83bb868b1c4
tu4mo <tu4mo@users.noreply.github.com>
2026-05-20 11:37:34 +0300

    Remove electron-store dependency and switch to localStorage for note management

--- app/main.js
+++ app/main.js
@@ -1,12 +1,9 @@
 import electron from 'electron'
 import path from 'path'
-import Store from 'electron-store'
 import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-
-Store.initRenderer()
 
 const { app, Menu, MenuItem, Tray } = electron
 const BrowserWindow = electron.BrowserWindow
--- app/preload.mjs
+++ app/preload.mjs
@@ -1,18 +1,14 @@
-import Store from 'electron-store'
 import debounce from 'lodash.debounce'
-
-const store = new Store()
 
 window.addEventListener('DOMContentLoaded', () => {
   const editor = document.getElementById('editor')
 
   editor.focus()
 
-  const note = store.get('note', '')
-  editor.innerText = note
+  editor.innerText = localStorage.getItem('note') ?? ''
 
   const saveNote = () => {
-    store.set('note', editor.innerText)
+    localStorage.setItem('note', editor.innerText)
   }
 
   editor.addEventListener('input', debounce(saveNote, 1000))
